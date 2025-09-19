99fedb2c72b3377ce73e6bf49a15ad5fbc2c35af
Alexander Graef <webmaster@portalzine.de>
2025-09-19 10:37:18 +0200

    v0.1.1-alpha

---
+++ build/SugarModules/custom/Extension/modules/Administration/Ext/Language/ru_ru.web_hooks_request.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * SugarCRM Community Edition is a customer relationship management program developed by
+ * SugarCRM, Inc. Copyright (C) 2004-2013 SugarCRM Inc.
+ *
+ * SuiteCRM is an extension to SugarCRM Community Edition developed by SalesAgility Ltd.
+ * Copyright (C) 2011 - 2018 SalesAgility Ltd.
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License version 3 as published by the
+ * Free Software Foundation with the addition of the following permission added
+ * to Section 15 as permitted in Section 7(a): FOR ANY PART OF THE COVERED WORK
+ * IN WHICH THE COPYRIGHT IS OWNED BY SUGARCRM, SUGARCRM DISCLAIMS THE WARRANTY
+ * OF NON INFRINGEMENT OF THIRD PARTY RIGHTS.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program; if not, see http://www.gnu.org/licenses or write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA.
+ *
+ * You can contact SugarCRM, Inc. headquarters at 10050 North Wolfe Road,
+ * SW2-130, Cupertino, CA 95014, USA. or at email address contact@sugarcrm.com.
+ */
+
+$mod_strings['LBL_WEB_HOOKS_REQUEST_HEADER'] = 'Веб Хуки';
+$mod_strings['LBL_WEB_HOOKS_REQUEST'] = 'Запросы Веб Хуков';
+$mod_strings['LBL_WEB_HOOKS_REQUEST_DESCRIPTION'] = 'Управление запросами Веб Хуков';
